43cf3266f8d51afa935cdbde18228dca98d9b958
Thanvi <77586167+thanvi47@users.noreply.github.com>
2022-05-26 13:47:49 +0600

    Add files via upload

--- Php_basic/db.php
+++ Php_basic/db.php
@@ -1,19 +1,25 @@
 <?php
+$hostname="localhost";
+ $servername="root";
+ $password="";
+ $dbname="blog";
+ $conn=mysqli_connect($hostname,$servername,$password,$dbname);
+ if(!$conn)echo 'error';
+// else echo 'done';
 
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "db";
+?>
 
-// Create a connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Die if connection was not successful
-if (!$conn){
-    die("Sorry we failed to connect: ". mysqli_connect_error());
+
+<?php
+function selectUser($id){
+
+
+
+}function selectAR($name){
+
+
+
 }
-else{ 
-  // Submit these to a database
-  // Sql query to be executed 
-  echo" Connection was successful <br>";
-}
+
+?>
